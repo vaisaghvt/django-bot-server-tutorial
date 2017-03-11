5e3b06fac01866e905180bbed659d59c7b45ed22
Vaisagh Air <vaisaghvt@gmail.com>
2017-03-11 22:49:11 +0800

    getting started with a telegram bot

---
+++ chatbot_tutorial/urls.py
@@ -0,0 +1,23 @@
+"""chatbot_tutorial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from .views import TelegramBotView
+
+urlpatterns = [
+	url(r'^c817304a3d163ebd58b44dd446eba29572300724098cdbca1a/?$', TelegramBotView.as_view()),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ chatbot_tutorial/views.py
@@ -0,0 +1,82 @@
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+import json
+import requests
+import random
+from django.utils.decorators import method_decorator
+from django.http.response import HttpResponse
+
+
+def get_chat_id(received_message):
+        chat_id = received_message['from']['id']
+        return chat_id
+
+
+def get_message_from_request(request):
+    
+    received_message = {}
+    decoded_request = json.loads(request.body.decode('utf-8'))
+    
+    if 'message' in decoded_request:
+        received_message = decoded_request['message']
+        
+        received_message['chat_id'] = get_chat_id(
+            received_message)
+
+    return received_message
+
+
+def send_messages(message, token):
+    # Ideally process message in some way. For now, let's just respond
+    jokes = {
+         'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                    """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat':    ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                    """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb':   ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
+                    """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] 
+         }  
+
+    post_message_url = "https://api.telegram.org/bot{0}/sendMessage".format(token)
+
+    result_message = {}
+    result_message['chat_id'] = message['chat_id']
+    if 'fat' in message['text']:
+        result_message['text'] = random.choice(jokes['fat'])
+    
+    elif 'stupid' in message['text']:
+        result_message['text'] = random.choice(jokes['stupid'])
+    
+    elif 'dumb' in message['text']:
+        result_message['text'] = random.choice(jokes['dumb'])
+
+    else:
+        result_message['text'] = "I don't know any responses for that. If you're interested in yo mama jokes tell me fat, stupid or dumb."
+
+    
+    response_msg = json.dumps(result_message)
+     
+    status = requests.post(post_message_url, headers={
+        "Content-Type": "application/json"}, data=response_msg)
+
+
+class TelegramBotView(generic.View):
+
+    # Dispatches to get or post 
+    # TODO: is the csrf_exempt necessary?
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+
+    # Post function to handle messages in whatever format they come
+    def post(self, request, *args, **kwargs):
+        TELEGRAM_TOKEN = '372895246:AAFdDs2JIoud745dGWoz8wSVlmxCKq5f6So'
+        message = get_message_from_request(request)
+
+        output_messages = send_messages(message, TELEGRAM_TOKEN)
+
+        return HttpResponse()
+
+    
+    
